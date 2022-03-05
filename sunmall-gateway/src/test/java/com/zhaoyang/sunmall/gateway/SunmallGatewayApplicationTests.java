3f5ba6134b733575be44c569aa157d0f902f2e93
ShiZhaoyang <shizhywin@qq.com>
2022-03-05 11:26:38 -0500

    Add gateway files.

---
+++ sunmall-gateway/src/test/java/com/zhaoyang/sunmall/gateway/SunmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoyang.sunmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunmallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
